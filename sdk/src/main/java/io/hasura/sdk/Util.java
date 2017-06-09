905cc1ca75ba1a21ad7221c55e18a732647a2858
Jaison Titus <jaison@hasura.io>
2017-06-09 17:10:59 +0530

    Added Log for raw body

--- sdk/src/main/java/io/hasura/sdk/Util.java
+++ sdk/src/main/java/io/hasura/sdk/Util.java
@@ -1,5 +1,7 @@
 package io.hasura.sdk;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
@@ -15,6 +17,7 @@ public class Util {
         int code = response.code();
         try {
             String rawBody = response.body().string();
+            Log.i("Response",rawBody);
             return gson.fromJson(rawBody, bodyType);
         } catch (JsonSyntaxException e) {
             String msg = "JSON structure not as expected. Schema changed maybe? : " + e.getMessage();
